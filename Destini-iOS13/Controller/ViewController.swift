cda503703ca663bd1f54b97c068f5777f56696aa
Berk <s.berkatik@gmail.com>
2020-08-10 12:24:42 +0300

    MVC Implemented
    MVC model is implemented. Structs are created for Story and StoryBrain.

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateQuestion()
     }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        storyBrain.nextDestination(userAnswer)
+        
+        sender.alpha = 0.5
+        
+        Timer.scheduledTimer(timeInterval:0.2, target:self, selector:#selector(updateQuestion), userInfo:nil, repeats: false)
+    }
+    
 
+    
+    @objc func updateQuestion() {
+        storyLabel.text = storyBrain.getQuestionDesc()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+        choice1Button.alpha = 1.0
+        choice2Button.alpha = 1.0
+    }
 
 }
 
